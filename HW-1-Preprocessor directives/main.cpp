fb788e8d798c2269c0f146fd4f5c45f0ba35175c
Ilya Jinx <j1nx.ed@yandex.ru>
2023-02-10 11:55:55 +0300

    quick fixes

--- HW-1-Preprocessor directives/main.cpp
+++ HW-1-Preprocessor directives/main.cpp
@@ -3,7 +3,7 @@
 /*
   10.02.2023
   Тема 2, Занятие 7
-  Работа 2, Макрос SUB
+  Работа 1, Препроцессорные директивы
   CPP-07
   @j1n4ed
 */
@@ -11,7 +11,13 @@
 #include <iostream>
 #include <Windows.h>
 
-#define SUB(x, y) (x - (y))
+#define MODE 1
+
+#ifndef MODE
+
+#error "MODE IS NOT DEFINED! DEFINE MODE"
+
+#endif
 
 int main(int argc, char** argv)
 {
@@ -19,12 +25,29 @@ int main(int argc, char** argv)
 	SetConsoleOutputCP(1251);
 	// ---------------------
 
-	int a = 6;
-	int b = 5;
-	int c = 2;
-	std::cout << SUB(a, b) << std::endl;
-	std::cout << SUB(a, b) * c << std::endl;
-	std::cout << SUB(a, b + c) * c << std::endl;
+	if (!MODE)
+	{
+		std::cout << "Работаю в режиме тренировки";
+	}
+	else if (MODE == 1)
+	{
+		std::cout << "Работаю в боевом режиме";
+
+		int x = 0, y = 0;
+
+		int add(int, int);
+
+		std::cout << "\nВведите число 1: ";
+		std::cin >> x;
+		std::cout << "Введите число 2: ";
+		std::cin >> y;
+		std::cout << "Результат сложения: " << add(x, y);
+	}
+	else
+	{
+		std::cout << "Неизвестный режим. Завершение работы";
+	}
+	
 
 	// clean && exit
 	std::cout << std::endl;
--- HW-2-Sub macross/main.cpp
+++ HW-2-Sub macross/main.cpp
@@ -1,1 +1,33 @@
-ECHO is on.
+// main.cpp
+
+/*
+  10.02.2023
+  Тема 2, Занятие 7
+  Работа 2, Макрос SUB
+  CPP-07
+  @j1n4ed
+*/
+
+#include <iostream>
+#include <Windows.h>
+
+#define SUB(x, y) (x - (y))
+
+int main(int argc, char** argv)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	// ---------------------
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+	// clean && exit
+	std::cout << std::endl;
+	system("pause");
+	return EXIT_SUCCESS;
+}
